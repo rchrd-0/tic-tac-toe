e8873a92c5b4dd4c1dae82bee64d4856cb5b153b
richard <richard.ytli+f@gmail.com>
2022-04-14 17:15:11 +0800

    Remove click event listeners when game ends

--- script.js
+++ script.js
@@ -13,6 +13,95 @@ const gameBoard = (() => {
   }
 })();
 
+const gameController = (() => {
+  const Player = (name, mark, turn) => {
+    return {
+      name,
+      mark,
+      turn
+    }
+  }
+  const p1 = Player('Player 1', 'X', true);
+  const p2 = Player('Player 2', 'O', false);
+
+  const getActivePlayer = () => (p1.turn) ? p1 : p2;
+  const playMove = (tile) => {
+    let mark = getActivePlayer().mark;
+    gameBoard.placeMark(tile, mark);
+    checkWin();
+  }
+  const checkWin = () => {
+    const board = gameBoard.getBoard();
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    const getWinner = (() => {
+      const players = {
+        p1: Object.assign({...p1}, {count: board.filter(mark => mark === p1.mark).length}),
+        p2: Object.assign({...p2}, {count: board.filter(mark => mark === p2.mark).length})
+      }
+      const checkWinFor = Object.keys(players)
+        .filter(p => players[p].count >= 3)
+        .map(p => players[p].mark);
+
+      return (function() {
+        let winningRow = {}
+        checkWinFor.forEach(mark => {
+          let indices = [];
+          for (let i = 0; i < board.length; i++) {
+            if (board[i] === mark) {
+              indices.push(i);
+            }
+          }
+          winConditions.forEach(condition => {
+            if (condition.every(index => indices.includes(index))) {
+              Object.assign(winningRow, {
+                mark: mark,
+                row: [...condition]
+              })
+            } 
+          })
+        })
+
+        if (Object.keys(winningRow).length > 0) {
+          let winner = Object.keys(players)
+            .filter (p => players[p].mark === winningRow.mark);
+          winner = Object.assign({...players[winner]}, {row: winningRow.row});
+          
+          return winner
+        }
+      })();
+    })();
+
+    (getWinner) ? endGame(getWinner) : toggleTurn();
+  }
+
+  const endGame = (winner) => {
+    ongoingGame = !ongoingGame;
+  }
+
+  let ongoingGame = true;
+  const isGameOngoing = () => {
+    return ongoingGame;
+  }
+  const toggleTurn = () => {
+    p1.turn = !p1.turn;
+    p2.turn = !p2.turn;
+  }
+
+  return {
+    playMove,
+    isGameOngoing
+  }
+})();
+
 const displayController = (() => {
   const gameTiles = document.querySelectorAll('.game-tile');
 
@@ -28,108 +117,15 @@ const displayController = (() => {
     if (!targetTile.textContent) {
       gameController.playMove(tileNum);
     }
-  }
 
+    if (!gameController.isGameOngoing()) {
+      gameTiles.forEach(tile => tile.removeEventListener('click', getTile))
+    }
+  }
   gameTiles.forEach(tile => tile.addEventListener('click', getTile))
 
   return {
-    renderBoard
-  }
-})();
-
-const gameController = (() => {
-  const Player = (name, mark, turn) => {
-    return {
-      name,
-      mark,
-      turn
-    }
-  };
-  const p1 = Player('Player 1', 'X', true);
-  const p2 = Player('Player 2', 'O', false);
-  const getActivePlayer = () => (p1.turn) ? p1 : p2;
-
-  const playMove = (tile) => {
-    let mark = getActivePlayer().mark;
-    gameBoard.placeMark(tile, mark);
-    checkWin();
-    toggleTurn();
-  }
-  const checkWin = () => {
-    const board = gameBoard.getBoard();
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-
-    const getWinner = (() => {
-      const players = {
-        p1: Object.assign({...p1}, {count: board.filter(mark => mark === p1.mark).length}),
-        p2: Object.assign({...p2}, {count: board.filter(mark => mark === p2.mark).length})
-      }
-      const checkWinFor = Object.keys(players)
-        .filter(p => players[p].count >= 3)
-        .map(p => players[p].mark);
-      // Array [X, O] if number of marks >= 3
-
-      return (function() {
-        let winningRow = {}
-        checkWinFor.forEach(mark => {
-          let indices = [];
-          for (let i = 0; i < board.length; i++) {
-            if (board[i] === mark) {
-              indices.push(i);
-            }
-          }
-          // [0, 1, 2] array index nums of each mark >= 3
-
-          winConditions.forEach(condition => {
-            if (condition.every(index => indices.includes(index))) {
-              Object.assign(winningRow, {
-                mark: mark,
-                row: [...condition]
-              })
-            }
-          })
-        })
-        
-        let winner = Object.keys(players)
-          .filter (p => players[p].mark === winningRow.mark);
-        winner = Object.assign({...players[winner]}, {row: winningRow.row});
-
-        return winner;
-      })();
-    })();
-
-    // const endGame = () => {
-    //   if (!board.includes(null)) {
-    //     console.log('Draw');
-    //   } else {
-    //     let player = (getWinner.player === p1.mark) ? p1.name : p2.name;
-    //     console.log(`${player} wins!`);
-    //   }
-    // }
-
-    // if (Object.keys(getWinner).length === 0 && board.includes(null)) {
-    //   toggleTurn()
-    // } else {
-    //   endGame();
-    // }
-
-  }
-  const toggleTurn = () => {
-    p1.turn = !p1.turn;
-    p2.turn = !p2.turn;
-  }
-
-  return {
-    playMove,
+    renderBoard,
   }
 })();
 
